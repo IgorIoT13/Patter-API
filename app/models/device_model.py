c9b951ca7cb7481d8486656234919ab416df03ee
Igor Co <igora.kuki@gmail.com>
2025-05-21 23:29:17 +0300

    Add device, data, location models

--- app/models/device_model.py
+++ app/models/device_model.py
@@ -4,8 +4,13 @@ class Device(db.Model):
     __tablename__ = 'devices'
 
     id = db.Column(db.Integer, primary_key=True)
+    data_id = db.Column(db.Integer, db.ForeignKey('device_data.id'), nullable=False)
+    location_id = db.Column(db.Integer, db.ForeignKey('locations.id'), nullable=False)
+    
     name = db.Column(db.String(80), unique=True, nullable=False)
-    type_id = db.Column(db.Integer, db.ForeignKey('device_types.id'), nullable=False)
+    type = db.Column(db.String(80), nullable=False)
+    topic = db.Column(db.String(80), nullable=False)
+    # type_id = db.Column(db.Integer, db.ForeignKey('device_types.id'), nullable=False)
 
     def __repr__(self):
         return f'<Device {self.name}>'
