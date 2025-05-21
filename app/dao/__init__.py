4abe33b9ca5c967ea9f97f5884a16c45cf6e4ef9
Igor Co <igora.kuki@gmail.com>
2025-05-22 01:21:31 +0300

    add struct for dao

---
+++ app/dao/__init__.py
@@ -0,0 +1,5 @@
+from .device_data_dao import DeviceDataDAO
+from .device_dao import DeviceDAO
+from .location_dao import LocationDAO
+from .user_dao import UserDAO
+from .brocker_server_dao import BrockerDAO
