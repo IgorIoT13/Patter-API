ebb5cbaaaa9aff5a124c1faf19146dc7e3f9c30a
Igor Co <igora.kuki@gmail.com>
2025-05-22 02:29:21 +0300

    fix logic to more simple in location dao

--- app/dao/location_dao.py
+++ app/dao/location_dao.py
@@ -10,17 +10,10 @@ class LocationDAO:
     @staticmethod
     def get_by_id(location_id: int) -> Location:
         location = Location.query.get(location_id)
-        
-        if location is None:
-            raise ValueError("Location not found")
-        
         return location
 
     @staticmethod
     def add(room: str, adress: str) -> Location:
-        if room is None or "" or " " or adress is None or "" or " " :
-            raise ValueError("Room and address cannot be None or empty")
-          
         new_location = Location(room=room, adress=adress)
         db.session.add(new_location)
         db.session.commit()
@@ -28,14 +21,7 @@ class LocationDAO:
 
     @staticmethod
     def update_data(location_id: int, room: str, adress: str) -> Location:
-        if room is None or "" or " " and adress is None or "" or " " :
-            raise ValueError("Room and address cannot be None or empty")
-        
         location = LocationDAO.get_location_by_id(location_id)
-        
-        if location is None:
-            raise ValueError("Location not found")
-
         if room is not None or "" or " ":     
             location.room = room
         if adress is not None or "" or " ":
@@ -46,9 +32,6 @@ class LocationDAO:
     
     @staticmethod
     def get_by_property(room: str, adress: str) -> Location:
-        if room is None or "" or " " or adress is None or "" or " " :
-            raise ValueError("Room and address cannot be None or empty")
-        
         location = Location.query.filter_by(room=room, adress=adress).first()
         return location
         
@@ -56,10 +39,6 @@ class LocationDAO:
     @staticmethod
     def delete(location_id: int) -> bool:
         location = LocationDAO.get_location_by_id(location_id)
-        
-        if location is None:
-            raise ValueError("Location not found")
-        
         db.session.delete(location)
         db.session.commit()
         return True
