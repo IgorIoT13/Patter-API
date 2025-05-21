9336898a38757edf944645c64102e9189c95de4b
Igor Co <igora.kuki@gmail.com>
2025-05-22 02:41:33 +0300

    Add user data

--- app/dao/user_dao.py
+++ app/dao/user_dao.py
@@ -1,2 +1,51 @@
 from app import db
 from app.models import User
+
+class UserDao:
+    @staticmethod
+    def get_all() -> list:
+        return User.query.all()
+    
+    @staticmethod
+    def get_by_id(user_id: int) -> User:
+        return User.query.get(user_id)
+    
+    @staticmethod
+    def get_by_username(username: str) -> User:
+        return User.query.filter_by(username=username).first()
+    
+    @staticmethod
+    def update_data(
+        user_id: int,
+        username: str = None,
+        password: str = None,
+        number: str = None
+    ) -> User:
+        user = UserDao.get_by_id(user_id)
+        
+        if username is not None:
+            user.username = username
+        if password is not None:
+            user.password = password
+        if number is not None:
+            user.number = number
+        
+        db.session.commit()
+        return user
+    
+    @staticmethod
+    def create(username: str, password: str, number: str) -> User:
+        user = User(
+            username=username,
+            password=password,
+            number=number
+        )
+        db.session.add(user)
+        db.session.commit()
+        return user
+    
+    @staticmethod
+    def delete(user_id: int) -> None:
+        user = UserDao.get_by_id(user_id)
+        db.session.delete(user)
+        db.session.commit()
