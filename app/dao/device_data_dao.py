d9955541ce390fa9611e6627c74192e3993d4c5c
Igor Co <igora.kuki@gmail.com>
2025-05-22 02:30:08 +0300

    fix logic to more simple in data dao

--- app/dao/device_data_dao.py
+++ app/dao/device_data_dao.py
@@ -11,8 +11,6 @@ class DeviceDataDao:
     
     @staticmethod
     def get_by_id(device_data_id: int) -> DeviceData:
-        if device_data_id is None:
-            raise ValueError("device_data_id cannot be None")
         return DeviceData.query.get(device_data_id)
     
     @staticmethod
@@ -23,12 +21,6 @@ class DeviceDataDao:
         humidity: Optional[float] = None 
     ) -> DeviceData:
         data = DeviceDataDao.get_device_data_by_id(device_data_id)
-        
-        if data is None:
-            raise ValueError("DeviceData not found")
-        if secure_status is None and temprature is None and humidity is None:
-            raise ValueError("At least one of secure_status, temprature, or humidity must be provided")
-        
         if secure_status is not None:
             data.secure_status = secure_status
         if temprature is not None:
@@ -55,8 +47,6 @@ class DeviceDataDao:
     @staticmethod
     def delete(data_id: int) -> None:
         data = DeviceDataDao.get_device_data_by_id(data_id)
-        if data is None:
-            raise ValueError("DeviceData not found")
         db.session.delete(data)
         db.session.commit()
     
