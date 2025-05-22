94f6e57258042185d90fc965d4c19b8a3f98267a
Igor Co <igora.kuki@gmail.com>
2025-05-22 03:12:00 +0300

    add file to test dao section

---
+++ app/tests/dao_test.py
@@ -0,0 +1,13 @@
+# from app import create_app, db
+# from app.dao import DeviceDataDao, DeviceDao, LocationDao, UserDao, BrockerDao
+
+# app = create_app()
+
+# def test_device_data_dao():
+#     with app.app_context():
+#         device_data = DeviceDataDao.get_all()
+#         assert device_data is not None
+#         assert isinstance(device_data, list)
+#         assert len(device_data) > 0
+#         print("DeviceDataDao test passed.")
+
