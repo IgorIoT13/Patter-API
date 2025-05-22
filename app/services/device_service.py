e68e0e8594e57e930a7dd827123eddc64257d91c
Igor Co <igora.kuki@gmail.com>
2025-05-23 01:21:25 +0300

    Add device service

---
+++ app/services/device_service.py
@@ -0,0 +1,91 @@
+from app.dao import DeviceDao
+from app.models import Device
+
+class DeviceService:
+    @staticmethod
+    def get_all() -> list:
+        return DeviceDao.get_all()
+
+    @staticmethod
+    def get_by_id(device_id: int) -> Device:
+        device = DeviceDao.get_by_id(device_id)
+        return device
+    
+    @staticmethod
+    def get_by_property(name: str = None, type: str = None, topic: str = None) -> Device:
+        device = DeviceDao.get_by_property(name, type, topic)
+        return device
+
+    @staticmethod
+    def create(name: str, type: str, topic: str, location_id: int) -> Device:
+        if name is None or type is None or topic is None or location_id is None:
+            raise ValueError("Name, type, topic and location_id cannot be None")
+        if DeviceDao.get_by_property(name, type, topic):
+            raise ValueError("Device already exists")
+        return DeviceDao.create(name, type, topic, location_id)
+
+    @staticmethod
+    def update(device_id: int, name: str = None, type: str = None, topic: str = None) -> None:
+        if device_id is None:
+            raise ValueError("Device ID cannot be None")
+        if device_id <= 0:
+            raise ValueError("Device ID must be a positive integer")
+        device = DeviceDao.get_by_id(device_id)
+        
+        if device is None:
+            raise ValueError("Device not found")
+        if name == device.name and type == device.type and topic == device.topic:
+            raise ValueError("No changes detected")
+        if name is None and type is None and topic is None:
+            raise ValueError("Name, type and topic cannot be None")
+        if name == "" or name == " " or type == "" or type == " " or topic == "" or topic == " ":
+            raise ValueError("Name, type and topic cannot be empty")
+        if DeviceService.get_by_property(name, type, topic):
+            raise ValueError("Device already exists")
+        
+        if name is not None and type is not None and topic is not None:
+            DeviceDao.update(device_id, name, type, topic)
+        elif name is not None and type is not None:
+            DeviceDao.update(device_id, name, type, device.topic)
+        elif name is not None and topic is not None:
+            DeviceDao.update(device_id, name, device.type, topic)   
+        elif type is not None and topic is not None:
+            DeviceDao.update(device_id, device.name, type, topic)
+        elif name is not None:
+            DeviceDao.update(device_id, name, device.type, device.topic)
+        elif type is not None:
+            DeviceDao.update(device_id, device.name, type, device.topic)
+        elif topic is not None:
+            DeviceDao.update(device_id, device.name, device.type, topic)
+            
+    @staticmethod
+    def delete(device_id: int, device: Device = None) -> None:
+        id = device_id
+        if device is not None:
+            id = device.id
+        elif device_id is None:
+            raise ValueError("Device ID cannot be None")
+        elif device_id <= 0:
+            raise ValueError("Device ID must be a positive integer")
+        
+        device = DeviceDao.get_by_id(device_id)
+        
+        if device is None:
+            raise ValueError("Device not found")
+        
+        DeviceDao.delete(device_id)
+    
+    @staticmethod
+    def get_by_location(location_id: int) -> list:
+        if location_id is None:
+            raise ValueError("Location ID cannot be None")
+        if location_id <= 0:
+            raise ValueError("Location ID must be a positive integer")
+        
+        devices = DeviceDao.get_all()
+        
+        for device in devices:
+            if device.location_id != location_id:
+                devices.remove(device)
+        return devices
+        
