65d71af5dd4405a3834670b2352c66a69b8bc24d
Igor Co <igora.kuki@gmail.com>
2025-05-23 01:18:55 +0300

    Add delete method

--- app/services/location_service.py
+++ app/services/location_service.py
@@ -1,4 +1,5 @@
-from app.dao import LocationDao
+from app.dao import LocationDao, DeviceDao
+from .device_service import DeviceService
 from app.models import Location
 
 class LocationService:
@@ -43,9 +44,6 @@ class LocationService:
         elif adress is not None:
             LocationDao.update(location_id, location.room, adress)
             
-            
-
-    
     @staticmethod
     def create(room: str = None, adress: str = None) -> Location:
         if room is None or adress is None:
@@ -53,3 +51,28 @@ class LocationService:
         if LocationDao.get_by_property(room, adress):
             raise ValueError("Location already exists")
         return LocationDao.create(room, adress)
+    
+    @staticmethod
+    def delete(location_id: int, location: Location = None) -> None:
+        id = location_id
+        if location is not None:
+            id = location.id
+        elif location_id is None:
+            raise ValueError("Location ID cannot be None")
+        elif location_id <= 0:
+            raise ValueError("Location ID must be a positive integer")
+        
+        location = LocationDao.get_by_id(location_id)
+        if location is None:
+            raise ValueError("Location not found")
+        
+        LocationDao.delete(location_id)
+        #TREBA DO PISATI VIDALENIA DEVICE
+        
+        
+        
+        
+        
+        
+        
+    
