36132df646f408021114e019013461e52e6abc1e
Igor Co <igora.kuki@gmail.com>
2025-05-23 01:34:44 +0300

    add temp test

--- test.py
+++ test.py
@@ -1,6 +1,7 @@
 from app import create_app, db
 from tests import Data, DeviceDataTest, DeviceTest, LocationTest, UserTest, BrockerTest
 from app.dao import DeviceDataDao
+from app.services import DeviceService, LocationService
 
 app = create_app()
 
@@ -17,8 +18,16 @@ def test_device_part_dao():
         BrockerTest.tests()
         UserTest.delete_test(id = Data.get_broker()["to_delete"]["user_id"])
         
+def test_device_service():
+    with app.app_context():
+        loc = LocationService.create(room="testServ", adress="testSecrvice")
+        LocationService.update(loc.id, room="testServUpd", adress="testSecrviceUpd")
+        dev = DeviceService.create(name="testServ", type="testServ", topic="testServ", location_id=loc.id)
+        DeviceService.update(dev.id, name="testServUpd", type="testServUpd", topic="testServUpd")
+        
 if __name__ == '__main__':
     prepare_test_environment()
-    test_device_part_dao()
+    test_device_service()
+    # test_device_part_dao()
     
 
