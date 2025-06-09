b23ecd57b34e81f1a043220dd2b8f3e38fda0fdb
Igor Co <igora.kuki@gmail.com>
2025-06-10 01:27:42 +0300

    Separete all to specifiled functions

--- test.py
+++ test.py
@@ -3,6 +3,9 @@ from tests import Data, DeviceDataTest, DeviceTest, LocationTest, UserTest, Broc
 from app.services import DeviceService, LocationService, DeviceDataService, UserService, BrockerService
 # import pymysql
 
+import random
+import time
+
 app = create_app()
 
 with app.app_context():
@@ -24,47 +27,45 @@ def test_device_part_dao():
         BrockerTest.tests()
         UserTest.delete_test(id = Data.get_broker()["to_delete"]["user_id"])
         
+def BaseLocationTablesServices():
+    LocationService.create("kitchen", "Lviv, Ukraine")
+    LocationService.create("living room", "Lviv, Ukraine")
+    LocationService.create("bedroom", "Lviv, Ukraine")
+    
+    LocationService.create("office", "Kyiv, Ukraine")
+    LocationService.create("garage", "Kyiv, Ukraine")
+    LocationService.create("garden", "Kyiv, Ukraine")
+    
+def BaseDeviceTablesServices(list_locations=None):
+    min = 1
+    max = len(list_locations) if list_locations else 1
+    
+    DeviceService.create("Kitchen Light", "Light", "kitchen/light", random.randint(min, max))
+    DeviceService.create("Living Room Fan", "Fan", "living_room/fan", random.randint(min, max))
+    DeviceService.create("Living Room Light", "Light", "living_room/light", random.randint(min, max))
+    DeviceService.create("Bedroom Light", "Light", "bedroom/light", random.randint(min, max))
+    
+    DeviceService.create("Office Light", "Light", "office/light", random.randint(min, max))
+    DeviceService.create("Garage Light", "Light", "garage/light", random.randint(min, max))
+    DeviceService.create("Garden Light", "Light", "garden/light", random.randint(min, max))
+
+def Device_test_service():
+    cur_device = DeviceService.get_by_property(name="test_device")
+    cur_location = LocationService.get_by_property(room="garden", adress="Kyiv, Ukraine")
+    DeviceService.update(cur_device.id, topic="empty")
+    DeviceService.update(cur_device.id, name="Garden Light Updated", location_id=cur_location.id)
+    time.sleep(15)
+    DeviceService.delete(cur_device.id)
+        
 def test_device_service():
     with app.app_context():
-        # loc = LocationService.create(room="testServ", adress="testSecrvice")
-        # LocationService.update(loc.id, room="testServUpd", adress="testSecrviceUpd")
+        BaseLocationTablesServices()
+        list_locations = LocationService.get_all()
+        BaseDeviceTablesServices(list_locations)
+        list_devices = DeviceService.get_all()
         
-        LocationService.create(room="testServ", adress="testServ")
-        LocationService.create(room="testServ1", adress="testServ3")
-        loc = LocationService.get_by_property(room="testServ1")
-        # loc_out = LocationService.get_by_property(room="testServUpd", adress="testServUpd1")
-        
-        # print(f"Local_out {loc_out.id}, {loc_out.adress}, {loc_out.room}")
-        LocationService.update(loc.id, room="testServUpd", adress="testServUpd1")
-        loc = LocationService.get_by_property(adress="testServUpd")
-        
-        # LocationService.update(-1)
-        # LocationService.get_by_property()
-        
-        
-        # dev = DeviceService.create(name="testServ", type="testServ", topic="testServ", location_id=loc.id)
-        # DeviceService.update(dev.id, name="testServUpd", type="testServUpd", topic="testServUpd")
-        # dev2 = DeviceService.create(name="testServ1", type="testServ", topic="testServ", location_id=loc.id)
-        # data1 = DeviceDataService.create(device_id=dev.id, secure_status=True, temprature=1.0, humidity=1.0)
-        # data2 = DeviceDataService.create(device_id=dev.id, secure_status=False, temprature=2.0, humidity=2.0)
-        # data3 = DeviceDataService.create(device_id=dev.id, secure_status=True, temprature=3.0, humidity=3.0)
-        # DeviceDataService.update(data1.id, device_id=dev.id, secure_status=False, temprature=4.0, humidity=4.0)
-        # DeviceDataService.update(data2.id, device_id=dev.id, secure_status=True, temprature=5.0, humidity=5.0)
-        # DeviceDataService.update(data3.id, device_id=dev.id, secure_status=False, temprature=6.0, humidity=6.0)
-
-        # user = UserService.create(username="testServ", password="testServ", number="testServ")
-        # userToDelete = UserService.create(username="testServ1", password="testServ", number="testServ")
-        # UserService.update(user.id, username="testServUpd", password="testServUpd", number="testServUpd")
-        
-        # brocker = BrockerService.create(dev.id, user.id)
-        # brocker2 = BrockerService.create(dev2.id, user.id)
-        # brocker_to_update = BrockerService.get_all_by_property(dev2.id, user.id)
-        # if brocker_to_update:
-        #     brocker_to_update = brocker_to_update[0]
-        #     BrockerService.update(id=brocker_to_update.id, id_user=userToDelete.id)
-        
-        # LocationService.delete(loc.id)
-        pass
+        # Device_test_service()        
+        # pass
         
 if __name__ == '__main__':
     prepare_test_environment()
