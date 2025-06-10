c18142f3c8209ebb247fd7fb852e1ff1344dd543
Igor Co <igora.kuki@gmail.com>
2025-06-10 03:08:48 +0300

    Fix data service

--- app/services/device_data_service.py
+++ app/services/device_data_service.py
@@ -1,34 +1,36 @@
 from app.models import DeviceData
 from app.dao import DeviceDataDao
+from moduls.tools import VariableTools, log_def
 
 class DeviceDataService:
+    __name__ = "DeviceDataService"
+    
+    @log_def(obj_name=__name__)
     @staticmethod
     def get_all() -> list:
         return DeviceDataDao.get_all()
 
+    @log_def(obj_name=__name__)
     @staticmethod
     def get_by_id(device_data_id: int) -> DeviceData:
+        
         device_data = DeviceDataDao.get_by_id(device_data_id)
         return device_data
 
+    @log_def(obj_name=__name__)
     @staticmethod
     def create(device_id: int, 
         secure_status: bool = False,
-        temprature: float = 0,
-        humidity: float = 0
+        temprature: float = -999.0,
+        humidity: float = -999.0
     ) -> DeviceData:
-        if device_id is None:
-            raise ValueError("Device ID cannot be None")
-        if device_id <= 0:
-            raise ValueError("Device ID must be a positive integer")
-        if secure_status is None:
-            raise ValueError("Secure status cannot be None")
-        if temprature is None or humidity is None:
-            raise ValueError("Temperature and humidity cannot be None")
+        VariableTools.check_id(device_id, "DeviceData")
+        VariableTools.one_can_be_not_none(secure_status, temprature, humidity)
+        VariableTools.least_must_be_not_none_float(temprature, humidity)
         device_data = DeviceDataDao.create(device_id, secure_status, temprature, humidity)
         return device_data
         
-    
+    @log_def(obj_name=__name__)
     @staticmethod
     def update(
         device_data_id: int,
@@ -37,40 +39,42 @@ class DeviceDataService:
         temprature: float = None,
         humidity: float = None
     ) -> DeviceData:
+        VariableTools.check_id(device_data_id, "DeviceData")
         data = DeviceDataDao.get_by_id(device_data_id)
-        ready_device_id = device_id if device_id is not None else data.device_id
-        ready_secure_status = secure_status if secure_status is not None else data.secure_status
-        ready_temprature = temprature if temprature is not None else data.temprature
-        ready_humidity = humidity if humidity is not None else data.humidity
+        VariableTools.least_must_be_not_none(
+            device_id, secure_status, temprature, humidity
+        )
+        if device_id is not None:
+            VariableTools.check_id(device_id, "Device")
+        device_id = VariableTools.compare_to_empty_int(data.device_id, device_id)
+        secure_status = VariableTools.compare_to_empty_bool(data.secure_status, secure_status)
+        temprature = VariableTools.compare_to_empty_float(data.temprature, temprature)
+        humidity = VariableTools.compare_to_empty_float(data.humidity, humidity)
         
-        if device_id is None and secure_status is None and temprature is None and humidity is None:
-            raise ValueError("At least one fild must be updated")
+        
         DeviceDataDao.update(
             device_data_id,
-            device_id=ready_device_id,
-            secure_status=ready_secure_status,
-            temprature=ready_temprature,
-            humidity=ready_humidity
+            device_id=device_id,
+            secure_status=secure_status,
+            temprature=temprature,
+            humidity=humidity
         )
         
+    @log_def(obj_name=__name__)
     @staticmethod
     def get_all_by_device_id(device_id: int) -> list:
-        if device_id is None:
-            raise ValueError("Device ID cannot be None")
-        if device_id <= 0:
-            raise ValueError("Device ID must be a positive integer")
+        VariableTools.check_id(device_id, "Device")
         device_data = DeviceDataDao.get_all()
+        
         if device_data is None:
             raise ValueError("Device data not found")
         result = [data for data in device_data if data.device_id == device_id]
         return result
-        
+    
+    @log_def(obj_name=__name__)
     @staticmethod
     def delete(device_data_id: int) -> None:
-        if device_data_id is None:
-            raise ValueError("Device data ID cannot be None")
-        if device_data_id <= 0:
-            raise ValueError("Device data ID must be a positive integer")
+        VariableTools.check_id(device_data_id, "DeviceData")
         device_data = DeviceDataDao.get_by_id(device_data_id)
         
         if device_data is None:
